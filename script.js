0bb784b328fec5292aa9cde3f0987f9d4a757aaf
Studdnet <st.codding@gmail.com>
2023-02-16 23:39:52 +0300

    fix: bug in header with Link{Home}

--- script.js
+++ script.js
@@ -1,7 +1,29 @@
 const menu = document.querySelector("#menu");
 const navbar = document.querySelector(".navbar");
+const icons = document.querySelectorAll(".navbar a");
+const iconsActive = document.querySelector("#active");
 
 menu.addEventListener("click", ()=> {
 	menu.classList.toggle("fa-times");
 	navbar.classList.toggle("active");
 });
+
+function linkHomeActive () {
+	if (!iconsActive.classList.contains("active")) {
+		iconsActive.classList.add("active");
+	}
+}
+
+icons.forEach(icon => {
+	icon.addEventListener("mouseover", ()=>{
+		iconsActive.classList.remove("active");
+	});
+});
+
+icons.forEach(icon => {
+	icon.addEventListener("mouseout", ()=>{
+		iconsActive.classList.add("active");
+	});
+});
+
+
